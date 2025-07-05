f4918b09aa0c395a8bd5a9eb04b84b10c899e5c7
Ayush Arya <dpsayush2004@gmail.com>
2025-07-05 20:52:13 +0530

    Updated to ResNet18 model and compatible app.py

--- app.py
+++ app.py
@@ -124,7 +124,9 @@ def predict():
         return jsonify({"error": "Unsupported file type"}), 400
 
 import os
-
+@app.route("/ping")
+def ping():
+    return jsonify({"status": "ok"})
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
